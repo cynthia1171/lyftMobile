25c16a8604f65b2e595f3fd0a0b585e84c2ee1d3
cynthia1171 <c.isla1171@gmail.com>
2017-12-13 22:55:53 -0300

    funcion limitar caracteres y generar codigo random

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   $('.modal').modal();
 
   $('select').material_select();
+
+  $('input#telephone').characterCounter();
 }); 
 
 /** función que hace que despues de que se vea el splash redireccione al index.html */
@@ -15,3 +17,22 @@ function load(){
     $(location).attr('href', 'index.html').fadeIn(500)
   }, 3000);
 }
+
+/**funcion para validar si los caracteres del input contiene 10 digitos */
+$('#telephone').keyup(function () {
+  var num = $('#telephone').val();
+  if (num.length == 10){
+    $('#next').removeClass('disabled');
+  } else if (num.length > 10){
+    $('#next').addClass('disabled');
+  } else if (num.length < 10) {
+    $('#next').addClass('disabled');
+  }
+});
+
+/**generar numero random */
+$('#next').click(function(){
+  var x = Math.floor((Math.random() * 1000) + 0);
+  document.getElementById("num").innerHTML = x;
+}); 
+  
