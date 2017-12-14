ffb9c495036ac790edeefa0d37a96d9ef3a57ecc
cynthia1171 <c.isla1171@gmail.com>
2017-12-13 23:01:06 -0300

    cambio nombre de paginas html

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,13 +8,11 @@ $(document).ready(function(){
 
 /** función que hace que despues de que se vea el splash redireccione al index.html */
 function load(){
-  var url = 'index.html';
-  /*splash*/
   setTimeout(function () {
     $('.splash').fadeOut(500);
   }, 3000);
   setTimeout(function () {
-    $(location).attr('href', 'index.html').fadeIn(500)
+    $(location).attr('href', 'principal.html').fadeIn(500)
   }, 3000);
 }
 
